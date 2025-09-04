93dcc8dfc829234464c612066d03af6b17323d89
Nico <nnobili@fi.uba.ar>
2025-09-03 23:55:27 -0300

    bug

--- train_scripts/Titan_train_Transformer.py
+++ train_scripts/Titan_train_Transformer.py
@@ -36,14 +36,14 @@ print("Device:", device)
 
 model_selection = 'Transformer_0309'
 epochs = 200
-lr = 0.5e-4
+lr = 1e-5
 batch_size = 32
 dataset = 'Dataset_Campo_10m_patched_MatchedHist_InputMatch' 
 low_res = '10m'
 losses = [nn.MSELoss()]
 losses_weights = [1]
 
-config = Swin2SRConfig(upscale=2, img_size=(32,32), window_size=8, img_range=1., depths=[6,6,6,6], embed_dim=54, num_heads=[6,6,6,6], mlp_ratio=4)
+config = Swin2SRConfig(upscale=2, img_size=(32,32), window_size=8, img_range=1., depths=[6,6,6,6,6,6], embed_dim=66, num_heads=[6,6,6,6,6,6], mlp_ratio=4)
 
 # ───────────────────────────────────────────────────────────────────────────────
 # 📁 Paths Setup
