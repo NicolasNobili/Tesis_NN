6186b1301caea68594946a672cd46ac67d2771f4
Nico <nnobili@fi.uba.ar>
2025-09-19 14:11:39 -0300

    a

--- train_scripts/Titan_train_RCAN_10m.py
+++ train_scripts/Titan_train_RCAN_10m.py
@@ -36,7 +36,7 @@ from project_package.utils.utils import serialize_losses
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
 
-model_selection = 'RCAN_0409'
+model_selection = 'RCAN_1909'
 
 epochs = 200
 lr = 1e-4
